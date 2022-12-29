54e5aded58ced2181106902125631b5b868893b2
Efe Furkan Aykut <116588595+EfeAykut@users.noreply.github.com>
2022-12-29 13:10:07 +0300

    creating contact info class

---
+++ src/main/java/com/example/cv_desktop_app/CI.java
@@ -0,0 +1,47 @@
+package com.example.cv_desktop_app;
+
+public class CI {
+
+    private String telephoneNo;
+    private String address;
+    private String email;
+
+    public CI(String telephoneNo, String address, String email) {
+        this.telephoneNo = telephoneNo;
+        this.address = address;
+        this.email = email;
+    }
+
+    public String getTelephoneNo() {
+        return telephoneNo;
+    }
+
+    public void setTelephoneNo(String telephoneNo) {
+        this.telephoneNo = telephoneNo;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "contactInfo{" +
+                "telephoneNo='" + telephoneNo + '\'' +
+                ", address='" + address + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
