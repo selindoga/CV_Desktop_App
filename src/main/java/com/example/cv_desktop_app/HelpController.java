c7dec3ba0f9dcbe791c86038fc19010096e4e8d1
selindoga <73192627+selindoga@users.noreply.github.com>
2023-01-04 19:59:59 +0300

    Controller changed

--- src/main/java/com/example/cv_desktop_app/HelpController.java
+++ src/main/java/com/example/cv_desktop_app/HelpController.java
@@ -2,12 +2,24 @@ package com.example.cv_desktop_app;
 
 import javafx.application.Application;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class HelpController extends Application {
 
     public Button backButton;
+    private Parent root;
+    private Stage stage;
+    private Scene scene;
     public static void main(String[] args) {
         launch(args);
     }
@@ -20,4 +32,5 @@ public class HelpController extends Application {
     @Override
     public void start(Stage primaryStage) {
     }
+
 }
